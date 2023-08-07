2c4113b298b1be3ee348a01ae99c190e5c8533a7
Omwomo <eugeneoduor79@gmail.com>
2023-08-07 11:54:53 +0300

    Submit the scores

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -30,3 +30,30 @@ const refreshScores = async () => {
     throw new Error('Error refreshing scores.');
   }
 };
+
+const submitScore = async (event) => {
+  event.preventDefault();
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+  const name = nameInput.value.trim();
+  const score = parseInt(scoreInput.value, 10);
+
+  if (!name || Number.isNaN(score)) {
+    throw new Error('Invalid input!');
+  }
+
+  if (!gameId) {
+    throw new Error('Game ID is not available.');
+  }
+
+  try {
+    await saveScore(gameId, name, score);
+    nameInput.value = '';
+    scoreInput.value = '';
+    await refreshScores();
+  } catch (error) {
+    throw new Error('Error saving the score.');
+  }
+};
+
+export { setGameId, refreshScores, submitScore };
