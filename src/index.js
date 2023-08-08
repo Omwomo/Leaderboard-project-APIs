e657a181aefa25d82a2b6bfcbd25a66d6474b442
Omwomo <eugeneoduor79@gmail.com>
2023-08-08 14:03:49 +0300

    Add function to toggle the screen from light theme to dark theme and vice-versa

--- src/index.js
+++ src/index.js
@@ -66,4 +66,19 @@ document.getElementById('submit').addEventListener('click', async (event) => {
   }
 });
 
+// Toggle between light and dark theme.
+
+const themeToggle = document.getElementById('theme-toggle');
+const { body } = document;
+
+themeToggle.addEventListener('click', () => {
+  if (body.classList.contains('dark-theme')) {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+  } else {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+  }
+});
+
 export { displayMessage, displayErrorMessage };
